3366d20036770aff8e276d3a223d8f58b7b94340
David Steven Polania Acosta <dpolania@bancolombia.com.co>
2019-01-17 13:31:16 -0500

    add new suppor for old reports

--- src/Extension/Report/Report.tsx
+++ src/Extension/Report/Report.tsx
@@ -52,7 +52,6 @@ class ReportContent extends React.Component<{}, IReportContentState> {
         try {
             
             const REPOSITORY_NAME: string = "DevOps_Vault_Reports_Extension";
-            const idTask = "13008e5e-9195-4043-9baf-c5d70da839e3";
             const configuration = await SDK.getConfiguration();
             const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
             const project = await projectService.getProject();
@@ -99,6 +98,7 @@ class ReportContent extends React.Component<{}, IReportContentState> {
 
             console.error("Error : ", err);
             this.setState({ message: " ¡ The report was not found !" });
+            await this.validateReportOld();
             
         }
     }
@@ -201,6 +201,59 @@ class ReportContent extends React.Component<{}, IReportContentState> {
                 }
             }
         });
+    }
+
+    private async validateReportOld() : Promise<void> {
+
+        try {
+            const idTask = "13008e5e-9195-4043-9baf-c5d70da839e3";
+            const configuration = await SDK.getConfiguration();
+            const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
+            const project = await projectService.getProject();
+
+            if(configuration && project ){
+
+                const release = configuration.releaseEnvironment;
+                const nameEnvironment =  release.name;
+                const RELEASE_DEFINITIONID = release.releaseDefinition.id;
+                const RELEASE_RELEASEID = release.releaseId;
+                const path = RELEASE_DEFINITIONID+"/"+RELEASE_RELEASEID+".html";
+
+                this.setState({ nameEnvironment: nameEnvironment });
+
+                let REPOSITORY_NAME = release.deployPhasesSnapshot.find(x => {
+                    return x.workflowTasks.find(y => y.taskId === idTask)
+                }).workflowTasks.find( x => x.taskId === idTask).inputs.nameRepository;
+
+                if(REPOSITORY_NAME){
+
+                    const client = getClient(GitRestClient);
+            
+                    let item:ItemReport = {
+                        url: "",
+                        commitId: Math.random().toString(36).substring(7),
+                        path: path,
+                        comment: "Report"
+                    }
+    
+                    item.content = await client.getItemText(REPOSITORY_NAME,item.path as string , project.id);
+    
+                    let items = [];
+                    items.push(item);
+    
+                    this.setState({ selectedTabId: items[0].commitId as string });
+                    this.setState({ items: items });
+
+                }else{
+        
+                    throw 'is valid if it belongs to an old repository but the repository was not found';
+                }
+            }
+
+        }catch (error){
+
+            console.error("Error : old repository : ", error);
+        }
     }
 }
 
